22e8b28db6796921858533e93950402250a29110
NicholasTavares <nicholasbalby@hotmail.com>
2024-11-15 11:03:03 -0300

    config nest and jest to use swc

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+// jest.config.js
+module.exports = {
+    moduleFileExtensions: ['js', 'json', 'ts'],
+    rootDir: 'src',
+    testRegex: '.*\\.spec\\.ts$',
+    transform: {
+        '^.+\\.(t|j)s$': ['@swc/jest'],
+    },
+    collectCoverageFrom: ['**/*.(t|j)s'],
+    coverageDirectory: "../coverage",
+    moduleNameMapper: {
+        "^src/(.*)$": "<rootDir>/../src/$1"
+    }
+};
